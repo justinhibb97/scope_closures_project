f9e732a52c258de93f495df18d70082966ea47be
Justin Hibbard <34048736+justinhibb97@users.noreply.github.com>
2023-10-24 18:51:02 -0400

    Add files via upload

---
+++ 17-arrow-reverse.js
@@ -0,0 +1,23 @@
+/***********************************************************************
+Write a function reverseStr(str) that accepts a string and returns that string
+reversed.
+
+Write this function using a fat arrow function!
+
+Examples:
+let result1 = reverseStr("hello"); // returns "olleh"
+let result2 = reverseStr("garden"); // returns "nedrag"
+let result3 = reverseStr("potato"); // returns "otatop"
+
+
+***********************************************************************/
+
+// Your code here
+let reverseStr = (str) => str.split("").reverse().join("");
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+    module.exports = reverseStr;
+  } catch (e) {
+    return null;
+  }
